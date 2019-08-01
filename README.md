3cf617dd6a1826dcd00e5d021318d520f710a532
Andrew Jessen-Tyler <andrewtyl@gmail.com>
2019-08-01 10:26:34 -0500

    copied previous solution

---
+++ README.md
@@ -0,0 +1,2 @@
+#callback-props_andrew-michael
+8-1 / Andrew Jessen-Tyler and Michael Calhoun / Callback Props
---
+++ src/Card.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Card from './Card';
+import renderer from 'react-test-renderer';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Card 
+    key={12}
+    title={'title'}
+    content={'content'}
+    />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders the UI as expected', () => {
+  const tree = renderer
+    .create(<Card />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();  
+  });
